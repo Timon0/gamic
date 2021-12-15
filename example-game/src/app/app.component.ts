df0e3029ff55db9b5ac5030e479281664d91f986
Timon Kurmann <timon.kurmann@gmail.com>
2021-12-15 04:43:38 +0100

    fix loading of the game

--- example-game/src/app/app.component.ts
+++ example-game/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { SmartContractService } from './core/services/smart-contract.service';
 
 @Component({
@@ -6,12 +6,21 @@ import { SmartContractService } from './core/services/smart-contract.service';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-    isAuthorized = false;
+export class AppComponent implements AfterViewInit {
+    isAuthorized: boolean | null = null;
 
     constructor(private smartContract: SmartContractService) {}
 
-    async ngOnInit() {
+    async ngAfterViewInit() {
         this.isAuthorized = await this.smartContract.hasNecessaryToken();
+        this.loadScript();
+    }
+
+    public loadScript() {
+        let node = document.createElement('script');
+        node.src = '/assets/mini-memory.min.js';
+        node.type = 'text/javascript';
+        node.async = true;
+        document.getElementsByTagName('head')[0].appendChild(node);
     }
 }
